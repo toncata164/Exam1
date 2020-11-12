ea59bf765265cc61087eb57021178de8a8606976
georgy-kirilov <georgynk17@gmail.com>
2020-11-12 18:31:07 +0200

    Added inRange and isNull methods in Validator

--- src/edu/school/exam/Validator.java
+++ src/edu/school/exam/Validator.java
@@ -2,8 +2,20 @@ package edu.school.exam;
 
 public class Validator 
 {
+	public static boolean isNull(Object value)
+	{
+		return value == null;
+	}
+	
 	public static boolean isNullOrEmpty(String value)
 	{
-		return value == null || value.length() == 0;
+		boolean nullOrEmpty = isNull(value) || value.length() == 0;
+		return nullOrEmpty;
+	}
+	
+	public static boolean isInRange(int actual, int min, int max)
+	{
+		boolean inRange = actual >= min && actual <= max;
+		return inRange;
 	}
 }
