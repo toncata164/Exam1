4d228366fc1c8d777e5eb885b91d4b24fdd47aa8
Asrock <esvalcheva@gmail.com>
2020-11-13 09:51:41 +0200

    pozdrav ot katq

--- src/edu/school/exam/Test.java
+++ src/edu/school/exam/Test.java
@@ -2,6 +2,7 @@ package edu.school.exam;
 
 public class Test 
 {
+	//pozdravi ot katq
 	public static void main(String[] args) 
 	{
 		Book book = new Book(null, "Denis", "Kushtata na Denis", "AHSJ1DK2SLS");
