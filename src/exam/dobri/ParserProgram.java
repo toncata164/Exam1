b41961abbbf466e55b187cfde2f27f762f79ba8e
2017_dobriig <2017_dobriig@LAB26-11.school.npmg.org>
2021-05-13 13:05:57 +0300

    New methods

--- src/exam/dobri/ParserProgram.java
+++ src/exam/dobri/ParserProgram.java
@@ -1,5 +1,7 @@
 package exam.dobri;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ParserProgram {
@@ -10,6 +12,37 @@ public class ParserProgram {
 				FileParser.Parse("src/exam/resources/police_killings.csv");
 		String name = people.get(0).getName();
 		System.out.println(name);
+		//firstKillenInMarch(people);
+		//latinKillers(people);
+		killedInMay(people);
 	}
-
+	public static void firstKillenInMarch(List<Person> people)
+	{
+		people.stream().
+		filter(x -> x.getYear() == 2015 && x.getMonth().equals("March"))
+		.map(x -> x.getName())
+		.limit(10)
+		.forEach(System.out::println);
+	}
+	public static void latinKillers(List<Person> people)
+	{
+	//	Comparator<Person> comp 
+	 //     = Comparator.comparing(Person::getAge);
+		people.stream().
+		filter(x -> x.getRaceethnicity().equals("Latino")
+				&& x.getMonth().equals("June") || x.getMonth().equals("July")
+				|| x.getMonth().equals("August"))
+		.map(x -> x.getName())
+		//.sorted(comp)
+		.forEach(System.out::println);
+	}
+	public static void killedInMay(List<Person> people)
+	{
+		int count = (int) people.stream().
+		filter(x -> x.getMonth().equals("May"))
+		.count();
+		
+		System.out.println(count);
+	}
+	
 }
