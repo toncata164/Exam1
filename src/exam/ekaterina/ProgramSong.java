134693092e81e6f89a327f960566cc249e5a6bb8
2017_ekaterinasv <2017_ekaterinasv@LAB26-4.school.npmg.org>
2021-06-02 10:34:31 +0300

    test

---
+++ src/exam/ekaterina/ProgramSong.java
@@ -0,0 +1,10 @@
+package exam.ekaterina;
+
+public class ProgramSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/exam/ekaterina/Song.java
@@ -0,0 +1,5 @@
+package exam.ekaterina;
+
+public class Song {
+
+}
